907ee46984943a86bb732ab975031f506e453e93
Charles Harvey <soops@ucla.edu>
2020-11-04 15:55:35 -0800

    fix missing declared vars

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "db_name" {}
 variable "resource_group_name" {}
 variable "db_sku_name" {}
 variable "db_version" {}
+variable "administrator_login" {}
+variable "administrator_login_password" {}
+variable "ssl_enforcement" {
+  default = true
+}
