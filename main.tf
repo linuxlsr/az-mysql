8c2b654095117cb9ced214b54da1730eda526975
Charles Harvey <soops@ucla.edu>
2020-11-04 15:50:41 -0800

    fix var name confusion

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_mysql_server" "mysql-server" {
   location = var.location
-  name = var.name
+  name = var.db_name
   resource_group_name = var.resource_group_name
   sku_name = var.db_sku_name
   version = var.db_version
